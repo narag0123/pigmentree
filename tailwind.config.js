c496ef19b202397c1563809ca86541e236e84a8f
narag0123 <narag0123@gmail.com>
2023-05-30 15:33:50 +0900

    커밋 메시지

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            black100: "#171717",
+            black80: "#434343",
+            black60: "#707070",
+            black40: "#9c9c9c",
+            black20: "#c9c9c9",
+
+            primary100: "#c8ff00",
+            primary80: "#d5ff3c",
+            primary60: "#ddfe65",
+            primary40: "#edffaa",
+            primary20: "#f6ffd6",
+
+            point: "#ad00ff",
+        },
+        extend: {
+            backgroundImage: {
+                "gradient-radial":
+                    "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
