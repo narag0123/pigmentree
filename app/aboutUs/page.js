00353135d2f3630cc030a148cd0e6ed6e10f4d1a
narag0123 <narag0123@gmail.com>
2023-06-13 19:31:40 +0900

    meta iamge

--- app/aboutUs/page.js
+++ app/aboutUs/page.js
@@ -3,6 +3,29 @@ import React from "react";
 import Container1 from "./container1/container1";
 import Container2 from "./container2/container2";
 
+export const metadata = {
+    openGraph: {
+        title: "ABOUT US",
+        description: "온라인 염료 판매 피그먼트리 입니다",
+        siteName: "Next.js",
+        images: [
+            {
+                url: "/favicon.ico",
+                width: 800,
+                height: 600,
+            },
+            {
+                url: "/favicon.ico",
+                width: 1800,
+                height: 1600,
+                alt: "My custom alt",
+            },
+        ],
+        locale: "en_KR",
+        type: "website",
+    },
+};
+
 function AboutUs() {
     return (
         <div className="aboutUs pt-[5rem]">
